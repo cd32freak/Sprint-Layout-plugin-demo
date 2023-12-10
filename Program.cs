f0eddc66f0433b9195dd44636a79635ad8624d30
cd32freak <58399150+cd32freak@users.noreply.github.com>
2023-12-10 16:20:02 +0100

    Minor improvements
    Added search and replace (all) option

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SprintLayoutPluginDemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Pass the command line arguments to the MainForm constructor
+            Application.Run(new MainForm(args));
+        }
+    }
+}
